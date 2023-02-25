b840499902331b273011a0da4a1cf9286bad46f1
boygialoc9x <52708792+boygialoc9x@users.noreply.github.com>
2023-02-26 00:10:22 +0700

    Dice

---
+++ Classes/Dice/Dice.cpp
@@ -0,0 +1,27 @@
+#include "Dice.h"
+
+// Constructor
+
+Dice::Dice() :
+m_pDice(nullptr)
+{
+
+}
+
+// Virtual
+
+bool Dice::init()
+{
+    return true;
+}
+
+std::string Dice::toString(int nTab)
+{
+    std::string ts;
+    return ts;
+}
+
+void Dice::log()
+{
+
+}
